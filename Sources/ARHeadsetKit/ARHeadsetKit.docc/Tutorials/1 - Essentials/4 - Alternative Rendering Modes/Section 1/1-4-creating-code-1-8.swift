0698e56b83a95d64e276ca701c939f4138a92e40
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-10-24 09:47:36 -0400

    Finished draft of tutorial 4 section 1

---
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/1 - Essentials/4 - Alternative Rendering Modes/Section 1/1-4-creating-code-1-8.swift
@@ -0,0 +1,74 @@
+import ARHeadsetKit
+import Metal
+
+class MyRenderer: CustomRenderer {
+    unowned let renderer: MainRenderer
+    var numFrames: Int = 0
+    
+    var objects: [ARObject] = []
+    
+    required init(renderer: MainRenderer, library: MTLLibrary!) {
+        self.renderer = renderer
+        
+        func createShape(shapeType: ARShapeType,
+                         position: simd_float3,
+                         scale: simd_float3,
+                         upDirection: simd_float3) -> ARObject
+        {
+            let yAxis: simd_float3 = [0.0, 1.0, 0.0]
+            let orientation = simd_quatf(from: yAxis, to: normalize(upDirection))
+            
+            return ARObject(shapeType: shapeType,
+                            position: position,
+                            orientation: orientation,
+                            scale: scale)
+        }
+        
+        // User-selected color
+        
+        objects.append(createShape(
+            shapeType: .squarePyramid,
+            position:    [0.00, 0.00, -0.26],
+            scale:       [0.08, 0.04,  0.04],
+            upDirection: [1.00, 2.00,  6.00])
+        )
+        
+        objects.append(createShape(
+            shapeType: .cylinder,
+            position:    [0.00, -0.12, -0.14],
+            scale:       [0.10,  0.04,  0.10],
+            upDirection: [0.00, -9.00,  1.00])
+        )
+        
+        // Static color
+        
+        objects.append(createShape(
+            shapeType: .sphere,
+            position:    [0.08, 0.00, -0.14],
+            scale:       [0.06, 0.06,  0.06],
+            upDirection: [0.00, 1.00,  0.00])
+        )
+        
+        objects.append(createShape(
+            shapeType: .cone,
+            position:    [-0.04, 0.00, -0.16],
+            scale:       [ 0.02, 0.10,  0.02],
+            upDirection: [ 3.00, 2.00,  2.00])
+        )
+    }
+    
+    func updateResources() {
+        numFrames += 1
+        
+        let red:     simd_float3 = [1.00, 0.00, 0.00]
+        let skyBlue: simd_float3 = [0.33, 0.75, 1.00]
+        
+        let coordinator = renderer.coordinator as! Coordinator
+        let renderingRed = coordinator.renderingRed
+        let color = renderingRed ? red : skyBlue
+    }
+    
+    func drawGeometry(renderEncoder: ARMetalRenderCommandEncoder) {
+        
+    }
+}
