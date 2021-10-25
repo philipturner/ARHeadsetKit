4475649ca5084243537fdcfd4b10b15a4b1d17e4
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-10-25 17:22:02 -0400

    Add tutorial 5 section 2

---
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/1 - Selecting Objects/Section 2/2-1-creating-code-2-6.swift
@@ -0,0 +1,25 @@
+import ARHeadsetKit
+
+extension GameRenderer: CustomRenderer {
+    
+    func updateResources() {
+        let mediumBlue: simd_float3 = [0.20, 0.50, 0.70]
+        let lightBlue:  simd_float3 = [0.60, 0.80, 1.00]
+        
+        var object = ARObject(shapeType: .cube,
+                              position: .zero,
+                              scale: .init(repeating: 0.2),
+                              
+                              color: mediumBlue)
+        
+        if let interactionRay = interactionRay,
+           object.trace(ray: interactionRay) != nil {
+            object.color = simd_half3(lightBlue)
+        }
+    }
+    
+    func drawGeometry(renderEncoder: ARMetalRenderCommandEncoder) {
+        
+    }
+    
+}
