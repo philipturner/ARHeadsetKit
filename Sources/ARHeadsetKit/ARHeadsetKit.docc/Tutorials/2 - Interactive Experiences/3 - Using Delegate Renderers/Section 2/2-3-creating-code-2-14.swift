394ab9e304f29e3c6e0021cf41962f9d596b0288
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-02 19:22:05 -0400

    Finish tutorial 7 section 2

--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/3 - Using Delegate Renderers/Section 2/2-3-creating-code-2-14.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/3 - Using Delegate Renderers/Section 2/2-3-creating-code-2-14.swift
@@ -1,8 +1,53 @@
-//
-//  File.swift
-//  
-//
-//  Created by Philip Turner on 11/2/21.
-//
+import ARHeadsetKit
 
-import Foundation
+class CubeRenderer: DelegateGameRenderer {
+    unowned let gameRenderer: GameRenderer
+    var cubes: [Cube] = []
+    
+    required init(gameRenderer: GameRenderer) {
+        self.gameRenderer = gameRenderer
+        
+        srand48(Int(Date().timeIntervalSince1970))
+        
+        for i in 0..<10 {
+            func frand24() -> Float {
+                Float(drand48())
+            }
+            
+            var location: simd_float3
+            print()
+            print("Cube \(i): Searching for location")
+            
+            repeat {
+                location = simd_float3(
+                    simd_mix(-0.3, 0.3, frand24()),
+                    simd_mix(-0.3, 0.3, frand24()),
+                    simd_mix(-0.6, 0.0, frand24())
+                )
+                
+                print("Generated potential location")
+            } while cubes.contains(where: { cube in
+                distance(cube.location, location) < 0.21
+            })
+            
+            var upDirection: simd_float3
+            print("Cube \(i): Searching for up direction")
+            
+            repeat {
+                upDirection = simd_float3(
+                    simd_mix(-1, 1, frand24()),
+                    simd_mix(-1, 1, frand24()),
+                    simd_mix(-1, 1, frand24())
+                )
+                
+                print("Generated potential up direction")
+            } while length(upDirection) > 1
+            
+            if length(upDirection) == 0 {
+                upDirection = [0, 1, 0]
+            }
+            
+            upDirection = normalize(upDirection)
+        }
+    }
+}
