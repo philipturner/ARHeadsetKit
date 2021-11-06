9b387a1249f9341baaa042849dc306cdf7f50705
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-06 17:18:38 -0400

    Finish tutorial 8 section 6

--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 6/2-4-creating-code-6-2.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 6/2-4-creating-code-6-2.swift
@@ -1,1 +1,27 @@
+import ARHeadsetKit
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        let description = Coordinator.AppDescription(name: "My App")
+        
+        ARContentView<EmptySettingsView>()
+            .environmentObject(Coordinator(appDescription: description))
+    }
+}
+
+class MyApp_MainRenderer: MainRenderer {
+    override var makeCustomRenderer: CustomRendererInitializer {
+        GameRenderer.init
+    }
+    
+    override class var interfaceDepth: Float {
+        0.45
+    }
+}
+
+class Coordinator: AppCoordinator {
+    override var makeMainRenderer: MainRendererInitializer {
+        MyApp_MainRenderer.init
+    }
+}
