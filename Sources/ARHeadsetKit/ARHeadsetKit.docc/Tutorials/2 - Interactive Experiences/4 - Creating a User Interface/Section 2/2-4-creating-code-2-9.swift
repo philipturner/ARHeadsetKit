512fc2978496b54caa25b9ed59af2207b029d694
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-06 10:26:58 -0400

    Finish tutorial 8 section 3

--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 2/2-4-creating-code-2-9.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 2/2-4-creating-code-2-9.swift
@@ -1,1 +1,60 @@
+import ARHeadsetKit
 
+extension GameInterface: ARParagraphContainer {
+    
+    enum CachedParagraph: Int, ARParagraphListElement {
+        case resetButton
+        case extendButton
+        
+        var parameters: Parameters {
+            switch self {
+            case .resetButton:  return ResetButton.parameters
+            case .extendButton: return ExtendButton.parameters
+            }
+        }
+    }
+    
+}
+
+
+
+fileprivate protocol GameInterfaceButton: ARParagraph { }
+
+extension GameInterfaceButton {
+    
+    typealias CachedParagraph = GameInterface.CachedParagraph
+    
+    static var paragraphWidth: Float { 0.15 }
+    static var pixelSize: Float { 0.25e-3 }
+    static var radius: Float { 0.02 }
+    
+    static var parameters: Parameters {
+        (stringSegments: [ (string: label, fontID: 2) ],
+         width: paragraphWidth, pixelSize: pixelSize)
+    }
+    
+    static func generateInterfaceElement(type: CachedParagraph) -> ARInterfaceElement {
+        let paragraph = GameInterface.createParagraph(type)
+        
+        let width  = 2 * radius + paragraphWidth
+        let height = 2 * radius + paragraph.suggestedHeight
+        
+        return ARInterfaceElement(
+            position: .zero, forwardDirection: [0, 0, 1], orthogonalUpDirection: [0, 1, 0],
+            width: width, height: height, depth: 0.05, radius: radius,
+            
+            highlightColor: [0.6, 0.8, 1.0], highlightOpacity: 1.0,
+            surfaceColor:   [0.3, 0.5, 0.7], surfaceOpacity: 0.75,
+            characterGroups: paragraph.characterGroups)
+    }
+    
+}
+
+fileprivate extension GameInterface {
+    
+    typealias Button = GameInterfaceButton
+    
+    enum ResetButton:  Button { static let label = "Reset Button" }
+    enum ExtendButton: Button { static let label = "Extend Button" }
+    
+}
--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 3/2-4-creating-code-3-4.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 3/2-4-creating-code-3-4.swift
@@ -1,1 +1,11 @@
+import ARHeadsetKit
 
+class GameInterface: DelegateGameRenderer {
+    unowned let gameRenderer: GameRenderer
+    
+    var buttons: ElementContainer!
+    
+    required init(gameRenderer: GameRenderer) {
+        self.gameRenderer = gameRenderer
+    }
+}
