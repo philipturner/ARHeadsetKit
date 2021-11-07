f49939e2139b1bd05f2e1fffbd656d0eda06783a
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-07 06:20:26 -0500

    Finish tutorial 8 section 8

--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 8/2-4-creating-code-8-11.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 8/2-4-creating-code-8-11.swift
@@ -1,1 +1,53 @@
+import ARHeadsetKit
 
+extension GameRenderer: CustomRenderer {
+    
+    func updateResources() {
+        cubePicker.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = true
+        }
+        
+        cubeRenderer.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = false
+            
+            if cubes[cubeIndex].velocity != nil {
+                cubePicker.cubeIndex = nil
+            }
+        }
+        
+        gameInterface.updateResources()
+    }
+    
+    func setReactionParams() {
+        let cubeIndex = cubePicker.cubeIndex!
+        let location = cubes[cubeIndex].location
+        
+        guard drand48() < 0.50 else {
+            gameInterface.reactionParams = nil
+            return
+        }
+        
+        let possibleMessages = [
+            "Score!",
+            "Rock On!",
+            "When Pigs Fly...",
+            "Yeet!",
+        ]
+        
+        var messageID = Int(arc4random())
+        messageID %= possibleMessages.count
+        
+        let message = possibleMessages[messageID]
+        gameInterface.reactionParams = (message, location)
+        gameInterface.updateReactionMessage()
+    }
+    
+    func drawGeometry(renderEncoder: ARMetalRenderCommandEncoder) {
+        
+    }
+    
+}
--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 8/2-4-creating-code-8-9.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 8/2-4-creating-code-8-9.swift
@@ -1,1 +1,39 @@
+import ARHeadsetKit
 
+extension GameRenderer: CustomRenderer {
+    
+    func updateResources() {
+        cubePicker.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = true
+        }
+        
+        cubeRenderer.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = false
+            
+            if cubes[cubeIndex].velocity != nil {
+                cubePicker.cubeIndex = nil
+            }
+        }
+        
+        gameInterface.updateResources()
+    }
+    
+    func setReactionParams() {
+        let cubeIndex = cubePicker.cubeIndex!
+        let location = cubes[cubeIndex].location
+        
+        guard drand48() < 0.50 else {
+            gameInterface.reactionParams = nil
+            return
+        }
+    }
+    
+    func drawGeometry(renderEncoder: ARMetalRenderCommandEncoder) {
+        
+    }
+    
+}
