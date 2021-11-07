3733b8eac9c0f717192c9426cf5958c966e1e287
Philip Turner <71743241+philipturner@users.noreply.github.com>
2021-11-07 08:00:17 -0500

    Finish tutorial 8 section 9

--- Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 9/2-4-creating-code-9-7.swift
+++ Sources/ARHeadsetKit/ARHeadsetKit.docc/Tutorials/2 - Interactive Experiences/4 - Creating a User Interface/Section 9/2-4-creating-code-9-7.swift
@@ -1,1 +1,63 @@
+import ARHeadsetKit
 
+extension GameRenderer: CustomRenderer {
+    
+    func updateResources() {
+        cubePicker.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = true
+        }
+        
+        cubeRenderer.updateResources()
+        
+        if let cubeIndex = cubePicker.cubeIndex {
+            cubes[cubeIndex].isRed = false
+            
+            if cubes[cubeIndex].velocity != nil {
+                if cubes.contains(where: {
+                    $0.velocity == nil
+                }) {
+                    setReactionParams()
+                }
+                
+                cubePicker.cubeIndex = nil
+            }
+        }
+        
+        gameInterface.updateResources()
+    }
+    
+    func setReactionParams() {
+        let cubeIndex = cubePicker.cubeIndex!
+        let location = cubes[cubeIndex].location
+        
+        guard drand48() < 0.50 else {
+            gameInterface.reactionParams = nil
+            return
+        }
+        
+        let possibleMessages = [
+            "Score!",
+            "Rock On!",
+            "When Pigs Fly...",
+            "Yeet!",
+        ]
+        
+        var messageID = Int(arc4random())
+        messageID %= possibleMessages.count
+        
+        let message = possibleMessages[messageID]
+        gameInterface.reactionParams = (message, location)
+        gameInterface.updateReactionMessage()
+    }
+    
+    func setCongratulation() {
+        
+    }
+    
+    func drawGeometry(renderEncoder: ARMetalRenderCommandEncoder) {
+        
+    }
+    
+}
